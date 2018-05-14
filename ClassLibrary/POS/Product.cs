a879e2ce2fcdd119d6e5c4051769b43b118f0fdf
rvnlvrz <rvnlvrz@gmail.com>
2018-05-14 19:27:19 +0800

    Add product class

---
+++ ClassLibrary/POS/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary.Inventory;
+
+namespace ClassLibrary.POS
+{
+    class Product
+    {
+        private int _id;
+        private List<Tuple<Item, int>> ingredients;
+        private decimal price;
+
+        public bool IsAvailable()
+        {
+            return ingredients.FindIndex(item => !item.Item1.IsAvailable()) != -1;
+        }
+    }
+}

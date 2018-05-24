27123d2a1df5fe411ed5efdf65015abf0323f92c
rvnlvrz <rvnlvrz@gmail.com>
2018-05-24 12:51:29 +0800

    Add product type property

--- ClassLibrary/Product.cs
+++ ClassLibrary/Product.cs
@@ -25,6 +25,7 @@ namespace ClassLibrary
         public int ID { get; set; }
         public string Name { get; set; }
         public string Price { get; set; }
+        public string Type { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<MenuItem> MenuItem { get; set; }
--- ClassLibrary/RestaurantModel.edmx.sql
+++ ClassLibrary/RestaurantModel.edmx.sql
@@ -2,7 +2,7 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 05/22/2018 19:52:01
+-- Date Created: 05/24/2018 12:50:44
 -- Generated from EDMX file: C:\Users\Arvin\source\repos\VelenicasRMS\ClassLibrary\RestaurantModel.edmx
 -- --------------------------------------------------
 
@@ -84,7 +84,8 @@ GO
 CREATE TABLE [dbo].[Products] (
     [ID] int IDENTITY(1,1) NOT NULL,
     [Name] nvarchar(max)  NOT NULL,
-    [Price] nvarchar(max)  NOT NULL
+    [Price] nvarchar(max)  NOT NULL,
+    [Type] nvarchar(max)  NOT NULL
 );
 GO
 
